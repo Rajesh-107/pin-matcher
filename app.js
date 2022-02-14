e009a5002f980a522a2ae21cfdbdb91505e476a0
Rajesh-107 <applerb326@gmail.com>
2022-02-14 23:27:52 +0600

    second commit

---
+++ app.js
@@ -0,0 +1,33 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 1000000);
+    const pinString = pin + '';
+    if (pinString.length == 5) {
+        return pin;
+    } else {
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click', function(event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    } else {
+
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+})
+
+function verifyPin() {
+
+}
